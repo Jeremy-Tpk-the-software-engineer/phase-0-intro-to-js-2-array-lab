d2c17871221db4cec7540506e508bdd4f665edad
Jeremy TOPAKA <jeremyemungu@gmail.com>
2022-09-12 05:11:37 -0400

    commit tree e2af4b8758cebccd3115f3eb078e7ac764f9de63

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const newArray = []
+    const cats = cats.concat(newArray)
+    cats.push(name)
+    return cats
+}
